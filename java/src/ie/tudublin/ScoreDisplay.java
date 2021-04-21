a81b406391bc09a78a951cc3688586a10fc16c4e
Fahyori18 <praifawss18@gmail.com>
2021-04-21 19:40:34 +0100

    finished loading note

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -2,14 +2,15 @@ package ie.tudublin;
 
 import java.util.ArrayList;
 
-
 import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
-	String score = "DEFGABcd";
-	//String score = "D2E2F2G2A2B2c2d2";
+	//String score = "DEFGABcd";
+	String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+
+	ArrayList<Note> note = new ArrayList<Note>();
 	
 	public void settings()
 	{
@@ -23,9 +24,39 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		loadScore();
+		printScores();
 	}
+	 
+	void loadScore(){
+ 
+		for( int i = 0; i < score.length(); i++)
+		{
+			char scoreNote = score.charAt(i); 
+			int  num;      
+			if(Character.isLetter(scoreNote))
+			{
+				if( i < score.length() - 1 && Character.isDigit(score.charAt(i+1)))
+				{
+					num = score.charAt(i + 1) - '0';
+				}
+				else
+				{
+					num = 1;
+				}
 
+				Note n = new Note(scoreNote, num);
+				note.add(n);
+			}
+		}
+ 	}
+
+	void printScores(){
+		for(Note n:note)
+		{
+			println(n);
+		}
+	}
 	public void draw()
 	{
 		background(255);
